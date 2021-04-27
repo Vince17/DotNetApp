67667b9c41ccccf8838c47877dc012a4bf8a67f9
Vincent B <vbeaughon.ir2022@esaip.org>
2021-04-27 17:24:53 +0200

    PushStartupTestV1

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -37,7 +37,7 @@ namespace WebApplication1
             services.AddRazorPages();
             services.AddSignalR();
         }
-        //Bonjour
+        //Bonjour Lucas et Juju
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
